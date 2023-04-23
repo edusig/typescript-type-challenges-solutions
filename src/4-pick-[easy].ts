e2290330d3d4a736b79be85f888ebb3c79d774bb
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2023-04-23 14:48:55 -0300

    feat: adds 4-pick solution

---
+++ src/4-pick-[easy].ts
@@ -0,0 +1,16 @@
+// Implementation
+type MyPick<T, K extends keyof T> = { [key in K]: T[K] };
+
+// Example
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = MyPick<Todo, "title" | "completed">;
+
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+};
